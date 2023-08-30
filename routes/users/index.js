cb9b236d223210184e0e3b94422cb29cdc301876
Alejandro <alejandro.av84@gmail.com>
2023-08-30 12:49:26 -0400

    adding crud and avg

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,22 +1,61 @@
-import {dbConnection} from "../../index.js";
-
-class UserModel {
-	async getUsers() {
-		const getAllUsersSql = 'select * from users';
-		const { rows } = await dbConnection.query(getAllUsersSql);
-		return rows;
-	}
-}
+import UserModel from './UserModel.js';
 
 class UserController {
-	constructor(UserModel) {
-		this.model = UserModel;
-	}
+  constructor(UserModel) {
+    this.model = UserModel;
+  }
 
-	async getUsers(req, res) {
-		const users = await this.model.getUsers()
-		res.send(users)
-	}
+  async createUser(req, res) {
+    const { ci, name, first_lastname, second_lastname, birth, age } = req.body;
+    if (!ci || !name || !first_lastname || !second_lastname || !birth || !age) {
+      res.status(400).send('A user required all fields, ci, name, first_lastname, second_lastname, birth, age');
+    }
+    //Todo validate user fields
+    const userCreated = await this.model.createUser(ci, name, first_lastname, second_lastname, birth, age);
+    res.status(201).send({ ...userCreated });
+  }
+
+  async editUser(req, res) {
+    const { id } = req.params;
+    if (!id) {
+      res.status(400).send('User ID is required');
+    }
+    const { ci, name, first_lastname, second_lastname, birth, age } = req.body;
+    if (!ci || !name || !first_lastname || !second_lastname || !birth || !age) {
+      res.status(400).send('A user required all fields, ci, name, first_lastname, second_lastname, birth, age');
+    }
+    //Todo validate user fields
+    const userEdited = await this.model.editUser(id, ci, name, first_lastname, second_lastname, birth, age);
+    res.status(200).send({ ...userEdited });
+  }
+
+  async getUsers(req, res) {
+    const users = await this.model.getUsers();
+    res.send(users);
+  }
+
+  async getUser(req, res) {
+    const { id } = req.params;
+    if (!id) {
+      res.status(400).send('User ID is required');
+    }
+    const users = await this.model.getUser(id);
+    res.status(200).send(users);
+  }
+
+  async deleteUser(req, res) {
+    const { id } = req.params;
+    if (!id) {
+      res.status(400).send('User ID is required');
+    }
+    await this.model.deleteUser(id);
+    res.status(204).send([]);
+  }
+
+  async getAvgAge(req, res) {
+    const avg = await this.model.usersAgeAvg();
+    res.status(200).send({ avg });
+  }
 }
 
 const model = new UserModel();
